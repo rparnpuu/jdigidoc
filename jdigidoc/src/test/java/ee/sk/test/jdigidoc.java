8bbe6d0ab25169d147ef31e66413eac368711f2e
Veiko Sinivee <veiko.sinivee@gmail.com>
2015-06-02 09:37:42 +0300

    #IB-3795 restore missing signer certs test/live verification in utility

--- jdigidoc/src/test/java/ee/sk/test/jdigidoc.java
+++ jdigidoc/src/test/java/ee/sk/test/jdigidoc.java
@@ -737,7 +737,8 @@ public class jdigidoc {
 					if(cv != null && DigiDocGenFactory.isTestCard(cert)) {
 						lerrs1.add(new DigiDocException(DigiDocException.ERR_TEST_SIGNATURE, "Test signature!", null));
 					}
-					
+					if(b)
+						  b = DigiDocVerifyFactory.verifySignatureFromLiveAndOcspFromTest(sig, lerrs1);
 					System.out.print("\tSignature: " + sig.getId() + " profile: " + sig.getProfile() + " - ");
 					System.out.print(cn);
 					if(!b && hasNonWarningErrs(m_sdoc, lerrs1))
