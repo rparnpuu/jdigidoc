87efbf667038ab7bb3939c8235206902e0eb1482
Rainer Villido <Rainer.Villido@nortal.com>
2016-03-14 18:55:14 +0200

    Changed maven groupId to ee.sk.digidoc; prepared maven pom file for releasing to Maven Central; added Maven Central release script

---
+++ jdigidoc/publish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+version="3.12.0"
+staging_url="https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+#staging_url=file:/Users/rainer/tmp/test-local-repo
+repositoryId="ossrh"
+module="jdigidoc"
+
+# Starting GPG agent to store GPG passphrase so we wouldn't have to enter the passphrase every time
+eval $(gpg-agent --daemon --no-grab)
+export GPG_TTY=$(tty)
+export GPG_AGENT_INFO
+
+artifact="target/$module-$version"
+echo "Deploying $artifact"
+mvn gpg:sign-and-deploy-file -DpomFile=pom.xml -Dfile=$artifact.jar -Durl=$staging_url -DrepositoryId=$repositoryId
+mvn gpg:sign-and-deploy-file -DpomFile=pom.xml -Dfile=$artifact-sources.jar -Dclassifier=sources -Durl=$staging_url -DrepositoryId=$repositoryId
+mvn gpg:sign-and-deploy-file -DpomFile=pom.xml -Dfile=$artifact-javadoc.jar -Dclassifier=javadoc -Durl=$staging_url -DrepositoryId=$repositoryId
+
+echo "Finished deployment"
+
+killall gpg-agent
